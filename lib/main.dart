353db35af9cb3f7ebc1dd593d30c77cd701cd737
Nilesh Telang <nileshtelang07n5@gmail.com>
2022-04-01 20:24:48 +0530

    Ques Commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,39 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  void corc() {
+    print('F U ');
+  }
 
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-      home: Material(
-        child: Center(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Title"),
+        ),
+        body: Center(
           child: Container(
-            child: Text("GodOfAvd Presents You >: "),
+            child: Column(
+              children: [
+                Text('Which Is Your Fav Anime ?'),
+                ElevatedButton(
+                  onPressed: corc,
+                  child: Text('Naruto'),
+                ),
+                ElevatedButton(
+                  onPressed: () => print('F U 2'),
+                  child: Text('One Piece'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print('F U 3');
+                  },
+                  child: Text('Hentai'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
