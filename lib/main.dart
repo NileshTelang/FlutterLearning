a52c1cd6f80281d2dc07f14dd3a8667ba251fde0
Nilesh Telang <nileshtelang07n5@gmail.com>
2022-04-02 13:34:16 +0530

    qna Commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,35 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  void corc() {
-    print('F U ');
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _qindex = 0, _aindex = 0;
+  void _corc() {
+    setState(() {
+      _qindex = _qindex + 1;
+      _aindex = 3;
+    });
+    print(_qindex + _aindex);
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = ['What\'s your fave anime ? ', 'Do you wanna die ?'];
+    var answers = [
+      'Naruto ',
+      ' One Piece ',
+      ' Hentai',
+      'yes ',
+      ' definitely yes ',
+      'Fuck'
+    ];
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
@@ -21,20 +43,20 @@ class MyApp extends StatelessWidget {
           child: Container(
             child: Column(
               children: [
-                Text('Which Is Your Fav Anime ?'),
+                Text(questions[_qindex]),
                 ElevatedButton(
-                  onPressed: corc,
-                  child: Text('Naruto'),
+                  onPressed: _corc,
+                  child: Text(answers[_aindex]),
                 ),
                 ElevatedButton(
                   onPressed: () => print('F U 2'),
-                  child: Text('One Piece'),
+                  child: Text(answers[_aindex + 1]),
                 ),
                 ElevatedButton(
                   onPressed: () {
                     print('F U 3');
                   },
-                  child: Text('Hentai'),
+                  child: Text(answers[_aindex + 2]),
                 ),
               ],
             ),
